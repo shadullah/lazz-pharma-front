999e47fb132c5dbfb7767a5a2ce67c533f74c2bc
hexaalif <shadullahsad9@gmail.com>
2024-08-15 02:55:29 +0600

    signup page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      login here
+      <Link href={"/register"}>Register</Link>
+    </div>
+  );
 };
 
 export default page;

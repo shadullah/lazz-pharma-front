74e28dede698976cccdc82f4e048d658c5573d1c
hexaalif <shadullahsad9@gmail.com>
2024-08-14 23:37:13 +0600

    file structured , products fetched

---
+++ src/components/FeaturedProducts.tsx
@@ -0,0 +1,106 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BackgroundGradient } from "./ui/background-gradient";
+import Image from "next/image";
+import Link from "next/link";
+import { Audio } from "react-loader-spinner";
+
+interface Product {
+  _id: string;
+  productImage: string;
+  name: string;
+  description: string;
+  price: number;
+  discount: number;
+  stock: boolean;
+}
+
+interface ApiResponse {
+  statusCode: number;
+  data: Product[];
+  message: string;
+}
+
+const FeaturedProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get<ApiResponse>(
+          "http://localhost:8000/api/v1/products/list"
+        );
+        console.log(res?.data);
+        setProducts(res?.data?.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
+            Featured Products
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+            {" "}
+            Get Best products here
+          </p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 justify-center m-12">
+          {loading ? (
+            <div className="flex justify-center">
+              <Audio
+                height="80"
+                width="80"
+                //   radius="9"
+                color="gray"
+                ariaLabel="loading"
+              />
+            </div>
+          ) : (
+            <>
+              {products?.map((product: Product) => (
+                <div className="flex justify-center" key={product?._id}>
+                  <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                    <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                      <Image
+                        src={product.productImage}
+                        alt="productImage"
+                        height="400"
+                        width="400"
+                        className="object-contain rounded-2xl mb-2"
+                      />
+                      <p className="text-3xl font-bold">{product.name}</p>
+
+                      <p className="text-justify my-2">{product.description}</p>
+                      <p>${product.price}</p>
+                      <Link
+                        href={"/:id"}
+                        className="bg-slate-700 w-full py-2 rounded-2xl text-sm"
+                      >
+                        Details
+                      </Link>
+                    </div>
+                  </BackgroundGradient>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
+const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className="p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Assist you with Medicine support
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive Lazz Pharma and bring happiness to your
+          life. Whether you want to know about a product or purchase it, we will
+          help and support you in evey struggle you face
+        </p>
+        <div className="mt-6">
+          <Button
+            borderRadius="1.75rem"
+            className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            Explore Store
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
